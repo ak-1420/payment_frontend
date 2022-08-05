a950e48fb44b732238a1d429560593bb00046298
Arun Kumar Kalakuntla <arunkumarkalakuntla2308@gmail.com>
2022-08-05 10:42:59 +0000

    signin signup header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
+      <Header />
+        <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/signin" element={<SignIn />} />
+           <Route path="/signup" element={<SignUp />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Header() {
+
+    const navigate = useNavigate();
+
+    const navigateToSignInPage = () => {
+           navigate('/signin')
+    }
+
+    const navigateToSignUpPage = () => {
+        navigate('/signup')
+ }
+
+
+    const navigateToHome = () => {
+        navigate('/')
+    }
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+          
+        <Toolbar >
+
+         <Typography sx={{cursor:'pointer'}} variant="h6" component="div" onClick={navigateToHome}>
+             Payment
+          </Typography>
+          <Box sx={{marginLeft : 'auto'}}>
+          <Button color="inherit" >Make a Transaction</Button>
+          <Button color="inherit" >Transactions List</Button>
+          <Button color="inherit" >Customers List</Button>
+          <Button color="inherit" onClick={navigateToSignInPage}>Sign In</Button>
+          <Button color="inherit" onClick={navigateToSignUpPage}>Sign Up</Button>
+          </Box>
+          
+        </Toolbar>
+         
+      </AppBar>
+    </Box>
+  );
+}
+
