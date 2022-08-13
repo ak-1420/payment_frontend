12b2215098e0d729e8dec912ef7fd54781345987
Arun Kumar Kalakuntla <arunkumarkalakuntla2308@gmail.com>
2022-08-13 07:12:31 +0000

    db setup and upto receiver done

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -4,16 +4,23 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-export default function DateInput({label}) {
-  const [value, setValue] = React.useState(null);
+export default function DateInput({label , defaultDate , setDate}) {
+  const [value, setValue] = React.useState(defaultDate ?? null);
+
+  const disableWeekends = (date) => {
+    return date.getDay() === 0 || date.getDay() === 6;
+  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DatePicker
+        disablePast={true}
+        shouldDisableDate={disableWeekends}
         label={label}
         value={value}
         onChange={(newValue) => {
           setValue(newValue);
+          setDate(newValue)
         }}
         renderInput={(params) => <TextField {...params} />}
       />
--- src/pages/TransactionForm.js
+++ src/pages/TransactionForm.js
@@ -2,9 +2,165 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import DateInput from '../components/DateInput';
-import { Button, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Select , Alert} from '@mui/material';
+import axios from 'axios';
+
 
 export default function TransactionForm() {
+
+  const [bookingDate , setBookingDate] = React.useState(null)
+
+  const [customers , setCustomers] = React.useState([])
+
+  const [receivers , setReceivers] = React.useState([])
+
+
+  React.useEffect(() => {
+    axios.get(`http://localhost:5000/receivers`).then((res) => {
+      setReceivers(res.data)
+      console.log(res.data , 'receivers data')
+    }).catch((err) => {
+      console.log('unable to fetch receivers data')
+    })
+  },[])
+
+  React.useEffect(() => {
+    axios.get(`http://localhost:5000/customers`).then((res) => {
+      setCustomers(res.data)
+      console.log(res.data , 'customers data')
+    }).catch((err) => {
+      console.log('unable to fetch customers data')
+    })
+  },[])
+
+  const [sender , setSender] = React.useState({
+    customer_id : "",
+    account_holder_name : "",
+    clear_balance : 0,
+    overdraft : ""
+  })
+
+  const [receiver , setReceiver] = React.useState({
+    bic : "",
+    institution : "",
+    account_holder_name : "",
+    account_holder_number : ""
+  })
+
+  const [transaction , setTransaction] = React.useState({
+    transfer_type : "",
+    message_code : "",
+    amount : "",
+    transfer_fees : "",
+    currency : ""
+  })
+
+  const [message , setMessage] = React.useState({})
+  const [amount , setAmount] = React.useState(null)
+  const [error , setError] = React.useState({
+    isError : null ,
+    message : ""
+  })
+
+  React.useEffect(() => {
+    // get customer record with the given 
+    const record = customers?.find((c) => c.id == sender?.customer_id)
+    
+    if(record){
+       setSender((prev) => ({
+         customer_id : record.id,
+         account_holder_name: record.name,
+         clear_balance : record.clear_balance,
+         overdraft : record.overdraft
+       }))
+       setError({
+        isError: false ,
+        message : ""
+      })
+    }
+    else
+    {
+      setError({
+        isError: true ,
+        message : "Customer Not Found"
+      })
+      setSender((prev) => ({
+        ...prev,
+        account_holder_name: "",
+        clear_balance : 0,
+        overdraft : ""
+      }))
+    }
+   
+    
+  },[sender.customer_id])
+
+  
+  const handleCustomerIdChange = (e) => {
+
+    const {value} = e.target
+
+    setSender((prev)=> ({
+      ...prev,
+      customer_id : value
+    }))
+  }
+
+  React.useEffect(() => {
+    // get receiver record with the given bic
+    const record = receivers?.find((r) => r.id == receiver?.bic)
+    if(record){
+
+      setReceiver((prev) => ({
+        ...prev,
+        institution : record.institution
+      }))
+
+      setError({
+        isError: false,
+        message: ""
+      })
+    }
+    else{
+      setError({
+        isError: true,
+        message: "Account Not Found"
+      })
+      setReceiver((prev) => ({
+        ...prev,
+        institution : ""
+      }))
+    }
+  },[receiver.bic])
+
+  const handleReceiverAcNameChange = (e) => {
+        const {value} = e.target
+        setReceiver((prev) => ({
+          ...prev,
+          account_holder_name : value
+        }))
+  }
+
+  const handleReceiverAcNoChange = (e) => {
+       const {value} = e.target
+       setReceiver((prev) => ({
+         ...prev,
+         account_holder_number : value
+       }))
+  }
+
+  const handleBicChange = (e) => {
+    const {value} = e.target 
+    setReceiver((prev) =>({
+      ...prev,
+      bic : value
+    }))
+  }
+
+  console.log(error , 'error data')
+  
+  console.log(receiver , 'receiver data')
+
   return (
       <div style={{padding: '20px'}}>
     <Box
@@ -17,18 +173,20 @@ export default function TransactionForm() {
     >
       <div>
         {/* Booking Date Input  */}
-       <DateInput label="Booking Date" />
+       <DateInput label="Booking Date" defaultDate={bookingDate} setDate={setBookingDate} />
 
 
         {/* sender details */}
        <div>
          <h4>Sender Details</h4>
-
+         {(error?.isError && error?.message != "") && <Alert severity="error">{error?.message}</Alert>}
          {/* input customer Id */}
          <TextField
           id="input-customer-id"
           label="Customer Id"
           type="text"
+          value={sender?.customer_id ?? ""}
+          onChange={handleCustomerIdChange}
         />
 
         {/* input customer accound holder name */}
@@ -36,6 +194,7 @@ export default function TransactionForm() {
           id="input-account-holder-name"
           label="Account Holder Name"
           type="text"
+          value={sender?.account_holder_name ?? ""}
           inputProps={{
             readOnly:true
           }}
@@ -46,6 +205,7 @@ export default function TransactionForm() {
           id="input-clear-balance"
           label="Clear Balance"
           type="number"
+          value={parseFloat(sender?.clear_balance) ?? 0}
           inputProps={{
             readOnly:true
           }}
@@ -53,8 +213,9 @@ export default function TransactionForm() {
 
          <TextField
           id="input-currency"
-          label="Currency"
+          label="Overdraft"
           type="text"
+          value={sender?.overdraft}
           inputProps={{
             readOnly:true
           }}
@@ -73,6 +234,8 @@ export default function TransactionForm() {
           id="input-bic"
           label="BIC"
           type="text"
+          value={receiver?.bic}
+          onChange={handleBicChange}
         />
 
         {/* input institution name*/}
@@ -80,6 +243,7 @@ export default function TransactionForm() {
           id="input-institution-name"
           label="Institution Name"
           type="text"
+          value={receiver?.institution ?? ""}
           inputProps={{
             readOnly:true
           }}
@@ -90,7 +254,8 @@ export default function TransactionForm() {
           id="input-receiver-ac-name"
           label="Account Holder name"
           type="text"
-          
+          value={receiver?.account_holder_name}
+          onChange={handleReceiverAcNameChange}
         />
 
          {/* input receiver account holder number */}
@@ -98,6 +263,8 @@ export default function TransactionForm() {
           id="input-account-holder-number-receiver"
           label="Acccount Holder Number"
           type="text"
+          value={receiver?.account_holder_number}
+          onChange={handleReceiverAcNoChange}
         />     
 
        </div>
@@ -162,7 +329,7 @@ export default function TransactionForm() {
           id="input-transfer-fees"
           label="Transfer fees"
           type="number"
-          iputProps={{
+          inputProps={{
             readOnly : true
           }}
         />  
